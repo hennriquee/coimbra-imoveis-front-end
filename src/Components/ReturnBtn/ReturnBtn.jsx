fe2c345057a534ad3d4ef77636f849a490f19a4c
BRUNO HENRIQUE MARTINS DA SILVA <bruno.silva@UBERTEC.LOCAL>
2025-09-19 09:14:45 -0300

    mobile adjusts

--- src/Components/ReturnBtn/ReturnBtn.jsx
+++ src/Components/ReturnBtn/ReturnBtn.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import "./return-btn.css";
 import returnImg from "../../assets/images/return-img.png";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ReturnBtn = () => {
-  const { pathname } = useLocation();
-
-  const imovelPage = pathname.includes("/catalogo/imovel") ? true : false;
-
-  const editImovel = pathname.includes("/adm/edit") ? true : false;
+  const navigate = useNavigate();
 
   return (
-    <Link
-      to={imovelPage ? "/catalogo" : editImovel ? "/adm/edit" : "/"}
-      className="return__btn"
-    >
+    <Link onClick={() => navigate(-1)} className="return__btn">
       <img src={returnImg} alt="< Retornar" />
     </Link>
   );
