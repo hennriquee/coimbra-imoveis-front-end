f4cb2ec46e350314f2e6fabc563170d257680974
BRUNO HENRIQUE MARTINS DA SILVA <bruno.silva@UBERTEC.LOCAL>
2026-01-27 14:22:10 -0300

    filtro por categoria

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -7,7 +7,7 @@ const Catalog = () => {
   const [imoveis, setImoveis] = useState([]);
   const [loading, setLoading] = useState(true); // estado de carregamento
   const [busca, setBusca] = useState("");
-  const inputBuscaRef = useRef();
+  const [filterCat, setFilterCat] = useState("");
 
   async function getImoveis() {
     try {
@@ -22,38 +22,73 @@ const Catalog = () => {
     getImoveis();
   }, []);
 
+  function formatText(text) {
+    return text
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+  }
+
   //BUSCA POR TITULO:
 
   const imoveisFiltrados = useMemo(() => {
-    const lowerBusca = busca.toLowerCase();
+    if (!imoveis) return [];
 
-    return busca === ""
-      ? imoveis
-      : imoveis.filter((imv) =>
-          (imv.title ?? "").toLowerCase().includes(lowerBusca),
-        );
-  }, [busca, imoveis]);
+    const lowerBusca = formatText(busca.toLowerCase());
+
+    return imoveis.filter((imv) => {
+      const matchBusca = formatText(imv.title ?? "")
+        .toLowerCase()
+        .includes(lowerBusca);
+
+      const matchCategoria = filterCat === "" || imv.category === filterCat;
+
+      return matchBusca && matchCategoria;
+    });
+  }, [busca, imoveis, filterCat]);
+
+  //FILTRO POR CATEGORIA:
+
+  const categoriasUnicas = useMemo(() => {
+    if (!imoveis) return [];
+
+    return [...new Set(imoveis.map((imv) => imv.category))];
+  }, [imoveis]);
 
   return (
     <section className="catalog__main">
       <h1>Catálogo</h1>
 
-      <form
-        className="catalog__search__form"
-        onSubmit={(e) => {
-          e.preventDefault();
-          inputBuscaRef.current?.blur(); // fecha teclado no mobile
-        }}
-      >
-        <input
-          className="catalog__input__busca"
-          type="search"
-          placeholder="Pesquisar"
-          value={busca}
-          ref={inputBuscaRef}
-          onChange={(ev) => setBusca(ev.target.value)}
-        />
-      </form>
+      <div className="filter__container">
+        <form
+          className="catalog__search__form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            inputBuscaRef.current?.blur(); // fecha teclado no mobile
+          }}
+        >
+          <input
+            className="catalog__input__busca"
+            type="search"
+            placeholder="Pesquisar"
+            value={busca}
+            onChange={(ev) => setBusca(ev.target.value)}
+          />
+        </form>
+        <select
+          value={filterCat}
+          onChange={(ev) => setFilterCat(ev.target.value)}
+          className="catalog__category__filter"
+        >
+          <option value="">Todos</option>
+          {categoriasUnicas.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
+      </div>
+
       {loading ? (
         <p className="loading__message">Carregando imóveis...</p> // mensagem enquanto busca
       ) : imoveis.length > 0 ? (
